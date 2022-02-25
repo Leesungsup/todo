12e62b19fa0398a086eeee9bc235dafc3e428291
Leesungsup <tmdtjq0116@naver.com>
2022-02-26 01:46:35 +0900

    Map

---
+++ kakaomap/src/MapContent.js
@@ -0,0 +1,36 @@
+/*global kakao*/
+import React, { Component } from "react";
+import styled from "styled-components";
+const API_KEY = process.env.REACT_APP_API_KEY;
+
+class MapContent extends Component {
+  componentDidMount() {
+    const script = document.createElement("script");
+    script.async = true;
+    script.src =
+      `https://dapi.kakao.com/v2/maps/sdk.js?appkey=${API_KEY}&autoload=false`;
+    document.head.appendChild(script);
+    script.onload = () => {
+      kakao.maps.load(() => {
+        let container = document.getElementById("Mymap");
+        let options = {
+          center: new kakao.maps.LatLng(37.506502, 127.053617),
+          level: 7
+        };
+  
+        const map = new window.kakao.maps.Map(container, options);
+     
+      });
+    };
+  }
+  render() {
+    return <MapContents id="Mymap"></MapContents>;
+  }
+}
+
+const MapContents = styled.div`
+  width: 100%;
+  height: 100%;
+`;
+
+export default MapContent;
--- kakaomap/src/index.js
+++ kakaomap/src/index.js
@@ -2,11 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import KakaoMap from './Pages/kakaoMap';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <KakaoMap />
   </React.StrictMode>,
   document.getElementById('root')
 );
